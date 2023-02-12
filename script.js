1a0b61a3e421e8bfabe06c61b3273374d09d6bdb
kijaguzoHC <116033557+chrispius006@users.noreply.github.com>
2023-02-12 21:06:21 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 const button= document.getElementById("but");
-const button2= document.getElementById("but2");
-const button3= document.getElementById("but3");
-const button4= document.getElementById("but4");
 const list= document.getElementById("list");
-const list2= document.getElementById("list2");
-const list3= document.getElementById("list3")
-const list4= document.getElementById("list4")
 const slider = document.querySelector(".slide");
 const nextBtn = document.querySelector(".next-btn");
 const prevBtn = document.querySelector(".prev-btn");
@@ -24,6 +18,7 @@ button.addEventListener("click",(Event)=>
         list.style.display= "none";
     }
 })
+
 
         nextBtn.addEventListener("click", () => {
           slides.forEach((slide) => {
@@ -95,3 +90,138 @@ button.addEventListener("click",(Event)=>
         slider.addEventListener("mouseout", () => {
           repeater();
         });
+
+ const element = document.querySelector("#count");
+const maxCount = 10;
+let count = 0;
+let hasCounted = false;
+
+const interval = setInterval(() => {
+    if (count <= maxCount && hasCounted) {
+            element.textContent = count;
+            count++;
+        }
+    if (count > maxCount){
+            clearInterval(interval);
+        }
+    }, 600);
+const card = document.querySelector("#card"); // Replace with the ID of your card element
+
+function handleScroll() {
+const cardPosition = card.getBoundingClientRect();
+    if (cardPosition.top <= window.innerHeight && !hasCounted) {
+            hasCounted = true;
+        }
+    }
+        window.addEventListener("scroll", handleScroll);
+        
+const element1 = document.querySelector("#count1");
+const maxCount1 = 17;
+let count1 = 0;
+let hasCounted1 = false;
+
+const interval1 = setInterval(() => {
+  if (count1 <= maxCount1 && hasCounted1) {
+    element1.textContent = count1;
+    count1++;
+  }
+  if (count1 > maxCount1) {
+    clearInterval(interval1);
+  }
+}, 500);
+const card1 = document.querySelector("#card1"); // Replace with the ID of your card element
+
+function handleScroll1() {
+  const cardPosition1 = card.getBoundingClientRect();
+  if (cardPosition1.top <= window.innerHeight && !hasCounted1) {
+    hasCounted1 = true;
+  }
+}
+window.addEventListener("scroll", handleScroll1);
+
+const element2 = document.querySelector("#count2");
+const maxCount2 = 14;
+let count2 = 0;
+let hasCounted2 = false;
+
+const interval2 = setInterval(() => {
+  if (count2 <= maxCount2 && hasCounted2) {
+    element2.textContent = count2;
+    count2++;
+  }
+  if (count2 > maxCount2) {
+    clearInterval(interval2);
+  }
+}, 400);
+
+const card2 = document.querySelector("#card2"); // Replace with the ID of your card element
+
+function handleScroll2() {
+  const cardPosition2 = card.getBoundingClientRect();
+  if (cardPosition2.top <= window.innerHeight && !hasCounted2) {
+    hasCounted2 = true;
+  }
+}
+window.addEventListener("scroll", handleScroll2);
+
+const element3 = document.querySelector("#count3");
+const maxCount3 = 10000;
+let count3 = 8000;
+let hasCounted3 = false;
+
+const interval3 = setInterval(() => {
+  if (count3 <= maxCount3 && hasCounted3) {
+    element3.textContent = count3;
+    count3++;
+  }
+  if (count3 > maxCount3) {
+    clearInterval(interval3);
+  }
+}, 0.01);
+
+const card3 = document.querySelector("#card3"); // Replace with the ID of your card element
+
+function handleScroll3() {
+  const cardPosition3 = card.getBoundingClientRect();
+  if (cardPosition3.top <= window.innerHeight && !hasCounted3) {
+    hasCounted3 = true;
+  }
+}
+window.addEventListener("scroll", handleScroll3);
+
+const h1 = document.querySelector('.ser h1');
+const p = document.querySelector('.ser p');
+const ser = document.querySelector('.ser');
+
+function checkSlide() {
+  const slideInAt = (window.scrollY + window.innerHeight) - h1.offsetHeight / 2;
+  const serBottom = ser.offsetTop + ser.offsetHeight;
+  const isHalfShown = slideInAt > ser.offsetTop;
+  const isNotScrolledPast = window.scrollY < serBottom;
+
+  if (isHalfShown && isNotScrolledPast) {
+    h1.classList.add('slide');
+    p.classList.add('slide2');
+  } else {
+    h1.classList.remove('slide');
+    p.classList.remove('slide2');
+  }
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
+
+// Debounce function to limit the rate at which the scroll event is fired
+function debounce(func, wait = 10, immediate = true) {
+  let timeout;
+  return function () {
+    const context = this, args = arguments;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
